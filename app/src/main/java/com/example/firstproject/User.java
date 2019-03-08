6b5a44c0e66e10b314fda99b4cbe1ee682d5576b
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-03-08 18:49:11 +0200

    [SP-21] Fix to camelCase and add fileconfig.yml

--- app/src/main/java/com/example/firstproject/User.java
+++ app/src/main/java/com/example/firstproject/User.java
@@ -3,68 +3,28 @@ package com.example.firstproject;
 import java.util.Date;
 
 public class User {
-    private int user_id;
-    private String user_name;
+    private int userId;
+    private String userName;
     private String password;
     private int balance;
-    private Date created_date;
+    private Date createdDate;
 
-    public User(int user_id, String user_name, String password, int balance, Date created_date) {
-        this.user_id = user_id;
-        this.user_name = user_name;
+    public User(int userId, String userName, String password, int balance, Date createdDate) {
+        this.userId = userId;
+        this.userName = userName;
         this.password = password;
         this.balance = balance;
-        this.created_date = created_date;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public Date getCreated_date() {
-        return created_date;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-
-    public void setCreated_date(Date created_date) {
-        this.created_date = created_date;
+        this.createdDate = createdDate;
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "user_id=" + user_id +
-                ", user_name='" + user_name + '\'' +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
                 ", password='" + password + '\'' +
                 ", balance=" + balance +
-                ", created_date=" + created_date +
+                ", createdDate=" + createdDate +
                 '}';
     }
 }
