e761d806bc5bde197fb2f570bd8be771e728e2d1
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-03-26 18:42:11 +0200

    [SP-68] makes button in main activity for comfortable crossing to WelcomeActivity

--- app/src/main/java/com/spendster/MainActivity.java
+++ app/src/main/java/com/spendster/MainActivity.java
@@ -3,6 +3,8 @@ package com.spendster;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.spendster.presentation.WelcomeActivity;
 
@@ -12,7 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, WelcomeActivity.class);
-        startActivity(intent);
+        Button btnStartWelcomeActivity = findViewById(R.id.btnStartWelcomeActivity);
+
+        btnStartWelcomeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
