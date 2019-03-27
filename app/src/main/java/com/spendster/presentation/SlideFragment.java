0073b9dbdd9ab83d3a1b0ff2c105c6485b6be12e
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-03-27 16:16:08 +0200

    [SP-20] makes 1 class SlideFragmenst instead of 3 separated classes

--- app/src/main/java/com/spendster/presentation/SlideFragment.java
+++ app/src/main/java/com/spendster/presentation/SlideFragment.java
@@ -6,12 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.spendster.R;
+public class SlideFragment extends Fragment {
+    private int slide;
 
-public class FirstFragment extends Fragment {
+    public static SlideFragment newInstance(int slide){
+        SlideFragment slideFragment = new SlideFragment();
+        slideFragment.slide = slide;
+        return slideFragment;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.slide1, container, false);
+        return inflater.inflate(this.slide , container, false);
     }
 }
