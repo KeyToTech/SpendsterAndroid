e2f4eddb5adef70bf911fc4252dfa78972f3c41d
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-04-02 13:39:51 +0300

    [SP-23] Creates login activity

---
+++ app/src/main/java/com/spendster/presentation/signUp/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.spendster.presentation.signUp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spendster.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
