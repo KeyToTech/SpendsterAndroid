e04f16c943d41773941cea03bfa4332ff5ed7443
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-04-02 15:26:24 +0300

    [SP-62] cleans up SignUpView

--- app/src/main/java/com/spendster/presentation/signUp/SignUpView.java
+++ app/src/main/java/com/spendster/presentation/signUp/SignUpView.java
@@ -3,14 +3,5 @@ package com.spendster.presentation.signUp;
 import com.spendster.presentation.AuthView;
 
 public interface SignUpView extends AuthView {
-    @Override
-    void showNextActivity();
-
-    @Override
-    void showEmailError(String message);
-
-    @Override
-    void showPasswordError(String message);
-
     void showRetypePasswordError(String message);
 }
