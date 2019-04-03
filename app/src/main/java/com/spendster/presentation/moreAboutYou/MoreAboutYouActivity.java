d4d48e634a8eeadab8240182dcb88d63df761e1c
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-04-03 16:06:10 +0300

    [SP-5] design for maybe later screen

---
+++ app/src/main/java/com/spendster/presentation/moreAboutYou/MoreAboutYouActivity.java
@@ -0,0 +1,15 @@
+package com.spendster.presentation.moreAboutYou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spendster.R;
+
+public class MoreAboutYouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_about_you);
+    }
+}
