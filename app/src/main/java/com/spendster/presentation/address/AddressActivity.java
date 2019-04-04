e03013e8af8403613d1f8196091182eb8e5a3d94
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-04-04 15:56:50 +0300

    [SP-5] initial commit

---
+++ app/src/main/java/com/spendster/presentation/address/AddressActivity.java
@@ -0,0 +1,15 @@
+package com.spendster.presentation.address;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spendster.R;
+
+public class AddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adress);
+    }
+}
