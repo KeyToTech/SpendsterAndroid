6ad6b16f9ef6d56624b7dee8038e055139d45d6f
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-04-16 18:49:31 +0300

    [SP-13] PageFragment

--- app/src/main/java/com/spendster/presentation/mainScreen/MainPagerAdapter.java
+++ app/src/main/java/com/spendster/presentation/mainScreen/MainPagerAdapter.java
@@ -1,6 +1,35 @@
 package com.spendster.presentation.mainScreen;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.List;
+
 public class MainPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> pageFragments;
+
+    public MainPagerAdapter(FragmentManager fm, List<Fragment> pageFragments) {
+        super(fm);
+        this.pageFragments = pageFragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) { return pageFragments.get(position); }
+
+    @Override
+    public int getCount() {
+        int result = 0;
+        if (this.pageFragments != null) {
+            result = this.pageFragments.size();
+        }
+        return result;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Title " + (position + 1);
+    }
 }
