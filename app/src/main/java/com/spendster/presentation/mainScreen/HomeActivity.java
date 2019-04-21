9de693e5ddd29a353f1c0c48ebfe0cad406a0985
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-04-21 22:20:48 +0300

    [SP-13] Adds splash screen

--- app/src/main/java/com/spendster/presentation/mainScreen/HomeActivity.java
+++ app/src/main/java/com/spendster/presentation/mainScreen/HomeActivity.java
@@ -29,6 +29,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         initUI();
