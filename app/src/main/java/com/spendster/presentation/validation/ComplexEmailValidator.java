709ffe9e26e79e5e25dec0975c1fef55915bd0dd
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-04-26 12:32:15 +0300

    [SP-126] Changes constants to upper case

--- app/src/main/java/com/spendster/presentation/validation/ComplexEmailValidator.java
+++ app/src/main/java/com/spendster/presentation/validation/ComplexEmailValidator.java
@@ -2,10 +2,10 @@ package com.spendster.presentation.validation;
 
 public class ComplexEmailValidator implements Validation<ValidationResource> {
     private final String email;
-    private final String containAt = ".*@.*";
-    private final String containTextBeforeAt = "^[a-zA-Z\\d_-]+@.+";
-    private final String containTextAfterAt = ".+@[a-zA-Z\\d_\\-\\.]+";
-    private final String containEnding = "^[a-zA-Z\\d_-]+@[a-zA-Z\\d_-]+\\..+";
+    private final String CONTAIN_AT = ".*@.*";
+    private final String CONTAIN_TEXT_BEFORE_AT = "^[a-zA-Z\\d_-]+@.+";
+    private final String CONTAIN_TEXT_AFTER_AT = ".+@[a-zA-Z\\d_\\-\\.]+";
+    private final String CONTAIN_ENDING = "^[a-zA-Z\\d_-]+@[a-zA-Z\\d_-]+\\..+";
 
     public ComplexEmailValidator(String email) {
         this.email = email;
@@ -19,16 +19,16 @@ public class ComplexEmailValidator implements Validation<ValidationResource> {
             errorMessage = "Email field is empty";
             isValid = false;
         }
-        else if (!this.email.matches(containAt)){
+        else if (!this.email.matches(CONTAIN_AT)){
             errorMessage = "Email must contain @ symbol";
             isValid = false;
-        }else if (!this.email.matches(containTextBeforeAt)){
+        }else if (!this.email.matches(CONTAIN_TEXT_BEFORE_AT)){
             errorMessage = "Email must contain text before @ symbol";
             isValid = false;
-        }else if (!this.email.matches(containTextAfterAt)) {
+        }else if (!this.email.matches(CONTAIN_TEXT_AFTER_AT)) {
             errorMessage = "Email must contain text after @ symbol";
             isValid = false;
-        }else if (!this.email.matches(containEnding)) {
+        }else if (!this.email.matches(CONTAIN_ENDING)) {
             errorMessage = "Email must contain something like .com";
             isValid = false;
         }
--- app/src/main/java/com/spendster/presentation/validation/ComplexSimpleValidator.java
+++ app/src/main/java/com/spendster/presentation/validation/ComplexSimpleValidator.java
@@ -2,7 +2,7 @@ package com.spendster.presentation.validation;
 
 public class ComplexSimpleValidator implements Validation<ValidationResource> {
     private final String username;
-    private final String correctSymbols = "\\w+";
+    private final String CORRECT_SYMBOLS = "\\w+";
 
     public ComplexSimpleValidator(String username) {
         this.username = username;
@@ -15,7 +15,7 @@ public class ComplexSimpleValidator implements Validation<ValidationResource> {
         if(username.isEmpty()){
             errorMessage = "Username field is empty";
             isValid = false;
-        }if(!username.matches(correctSymbols)){
+        }if(!username.matches(CORRECT_SYMBOLS)){
             errorMessage = "Forbidden symbols in username";
             isValid = false;
         }
