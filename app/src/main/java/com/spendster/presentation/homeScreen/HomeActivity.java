874623bdd53c0e1667c6602595faf202ded6bf08
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-05-02 23:02:40 +0300

    [SP-123] Adds LogOut button

--- app/src/main/java/com/spendster/presentation/homeScreen/HomeActivity.java
+++ app/src/main/java/com/spendster/presentation/homeScreen/HomeActivity.java
@@ -1,10 +1,11 @@
 package com.spendster.presentation.homeScreen;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.spendster.R;
+import com.spendster.presentation.welcomeScreen.WelcomeActivity;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -32,24 +34,29 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         initButtons();
     }
 
-    private void initButtons(){
+    private void initButtons() {
         Button btnAdd = findViewById(R.id.btnAdd);
+        Button btnLogOut = findViewById(R.id.btnLogOut);
         btnAdd.setOnClickListener(this);
+        btnLogOut.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btnAdd:
                 Toast.makeText(this, "Add Expenses", Toast.LENGTH_SHORT).show();
+            case R.id.btnLogOut:
+                startActivity(new Intent(HomeActivity.this, WelcomeActivity.class));
         }
+
 
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         Fragment fragment = null;
-        switch (menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.activityItem:
                 fragment = DashboardFragment.newInstance();
                 break;
@@ -67,8 +74,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         return loadFragment(fragment);
     }
 
-    private boolean loadFragment(Fragment fragment){
-        if (fragment != null){
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.fragment_container, fragment)
