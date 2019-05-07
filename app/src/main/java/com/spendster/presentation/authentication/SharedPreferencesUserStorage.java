108ba71d64e3f1aeffe10b83df454f2727c649c7
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-05-07 17:38:07 +0300

    [SP-90] extracts constants and makes exaption

--- app/src/main/java/com/spendster/presentation/authentication/SharedPreferencesUserStorage.java
+++ app/src/main/java/com/spendster/presentation/authentication/SharedPreferencesUserStorage.java
@@ -2,16 +2,22 @@ package com.spendster.presentation.authentication;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.spendster.data.entity.User;
 
+import java.util.NoSuchElementException;
+
 public class SharedPreferencesUserStorage implements SUserStorage {
+
+    private final String USER_EXIST = "user_exist";
+    private final String USER_SAVED = "user_saved";
 
     private SharedPreferences sharedPreferences;
 
     public SharedPreferencesUserStorage(Context context) {
-        this.sharedPreferences = context.getSharedPreferences("user_exist", Context.MODE_PRIVATE);
+        this.sharedPreferences = context.getSharedPreferences(USER_EXIST, Context.MODE_PRIVATE);
     }
 
     @Override
@@ -19,24 +25,32 @@ public class SharedPreferencesUserStorage implements SUserStorage {
         Gson gson = new Gson();
         String json = gson.toJson(user);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("user_saved", json);
+        editor.putString(USER_SAVED, json);
         editor.apply();
     }
 
     @Override
     public User read() {
         Gson gson = new Gson();
-        String json = sharedPreferences.getString("user_saved", "");
-        User user = null;
-        if (!json.isEmpty()){
+        String json = sharedPreferences.getString(USER_EXIST, "");
+        User user = new User();
+        if (!json.isEmpty()) {
             user = gson.fromJson(json, User.class);
+        } else {
+            throw new NoSuchElementException("User is not found");
         }
         return user;
     }
 
     @Override
     public boolean userExist() {
-        return this.read() != null;
+        boolean exist = false;
+        try {
+            exist = read() != null;
+        } catch (NoSuchElementException e) {
+            Log.d("Error", e.getMessage());
+        }
+        return exist;
     }
 
     @Override
