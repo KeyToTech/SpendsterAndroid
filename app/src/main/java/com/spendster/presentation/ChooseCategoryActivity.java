11d30bb254bbedf974105531c37b00a72f6f1d95
Sviatoslav Maha <svatoslavmaga@gmail.com>
2019-05-08 18:22:35 +0300

    [SP-135] initial commit

---
+++ app/src/main/java/com/spendster/presentation/ChooseCategoryActivity.java
@@ -0,0 +1,15 @@
+package com.spendster.presentation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.spendster.R;
+
+public class ChooseCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_category);
+    }
+}
